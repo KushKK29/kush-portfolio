ff061003e635263cda8c45272954ef86e0ae389d
KushKK29 <kush282930@gmail.com>
2025-03-01 23:11:19 +0530

    asdjbkjb

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/kush-portfolio/", // Change this to your actual repo name
   plugins: [react()],
 });
-
